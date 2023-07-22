e31a3bd0ad8ae6cd874fe83bba75eb15852f108f
Vamsi <saivamsi7996@gmail.com>
2023-07-22 02:00:42 -0400

    ng_template

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -12,16 +12,25 @@ export class CheckoutComponent {
     @Input() addList:number = 0;
     @Output() updateItem = new EventEmitter();
     finalList:Fruits[] = [];
+    display = true;
+    checkoutCount:boolean = false;
 
     constructor(private router: Router, private allf: AllfrutisService){}
 
     ngOnInit(){
-     this.finalList = this.allf.getItem();
+      this.finalList = this.allf.getItem();
+      if(this.finalList.length != 0){
+        this.checkoutCount = true;
+      }
     }
 
     removeItem(){
       this.addList = Math.abs(this.addList - 1);
      this.updateItem.emit(this.addList);
+    }
+
+    totalAmount(){
+      console.log(this.finalList);
     }
 
     navItem(){
